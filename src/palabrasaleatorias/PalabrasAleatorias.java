7618fbd5c965906ff082f3b9f33b5e094f37516e
JesusAlvarez <jjalvarezgar@gmail.com>
2019-10-11 13:05:11 +0200

    Palabras aleatoria para ejercicio 1 procesos, 11 DE OCTUBRE

--- src/palabrasaleatorias/PalabrasAleatorias.java
+++ src/palabrasaleatorias/PalabrasAleatorias.java
@@ -11,24 +11,71 @@ package palabrasaleatorias;
  */
 public class PalabrasAleatorias {
 
+      
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        
+        int numeroPalabras=0;
         int numeroLetras=0;
         String palabra="";
-        numeroLetras = (int) (Math.random()*10+1);
+        String nombreFichero;
+           // Si tenemos un argumento, el primero es el número de palabras a generar
+        if (args.length > 0) {
+            numeroPalabras = Integer.parseInt(args[0]);
+        } else {
+            numeroPalabras = 10;
+        }
+        //Identificamos el sistema operativo
+        String osName = System.getProperty("os.name");
+        
+        if (osName.toUpperCase().contains("WIN")) { 
+            //Windows
+            if (args.length > 1) {
+                // Si tenemos segundo argumento corregimos la dirección
+                 nombreFichero = args[1].replace("\\", "\\\\");
+            } 
+            else {
+                 nombreFichero = ".\\lenguaje.txt";
+                //Fichero si se omite el deseado
+            }
+        } else { 
+            //GNU/Linux
+            if (args.length > 1) {
+                // Si tenemos segundo argumento
+                 nombreFichero = args[1];
+            } 
+            else {
+                nombreFichero = "./lenguaje.txt";
+                //Fichero si se omite el deseado
+            }
+        }
+        
         //System.out.println(numeroLetras);
-        for (int i = 0; i < numeroLetras; i++) {
+        for (int j = 0; j < numeroPalabras; j++) {
             
-            int palabraAscii = (int) (Math.random()*(123-97+1)+97);//(N-M+1)+M
+            palabra="";
+            numeroLetras = (int) (Math.random()*10+1);
+              for (int i = 0; i < numeroLetras; i++) {
+            
+            // 123 es el código ASCII del la z
+            // y 97 es el código ASCII de la a 
+            // Para hallar un numero comprendido entre estos 
+            // hay que utilizar esta fórmula: (numeroMayor-numeroMenor+1)+numeroMenor
+            
+            int palabraAscii = (int) (Math.random()*(122-97+1)+97);
             palabra+=(char) palabraAscii;
             
-            System.out.println(palabraAscii);
+            
             
         }
         System.out.println(palabra);
+        
+            
+        }
+      
 
     }
     
